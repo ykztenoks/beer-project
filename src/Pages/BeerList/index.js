b19c77eee8c9d24f53f74ed10462657f6c809446
ykztenoks <danielskonetzky7@gmail.com>
2022-07-04 12:00:48 -0300

    added home and list

--- src/Pages/BeerList/index.js
+++ src/Pages/BeerList/index.js
@@ -1,7 +1,38 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Card } from "../../Components/Card";
+import { ButtonBack } from "../../Components/ButtonBack";
+import { Button } from "bootstrap";
 export function BeerList() {
+  const [beer, setBeer] = useState([]);
+
+  useEffect(() => {
+    async function fetchBeer() {
+      try {
+        const response = await axios.get(
+          "https://ironrest.herokuapp.com/mocked-beers"
+        );
+        setBeer(response.data[0].beer);
+        console.log(response);
+        console.log(response.data[0].beer);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchBeer();
+  }, []);
+
   return (
     <div>
       <h1>Beer List</h1>
+      <ButtonBack />
+      {beer.map((currentBeer) => {
+        return (
+          <div>
+            <Card beer={currentBeer} />
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
+import { SearchBar } from "../../Components/SearchBar";
+import { NavBar } from "../../Components/NavBar";
+import { Banner } from "../../Components/Banner";
 export function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <SearchBar />
+      <NavBar />
+      <Banner />
     </div>
   );
 }
