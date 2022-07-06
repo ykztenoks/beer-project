1284e9842b968322a54bd269ac583395cf369145
ykztenoks <danielskonetzky7@gmail.com>
2022-07-06 18:15:01 -0300

    searchbar working

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,34 +1,32 @@
-import style from "./style.module.css"
+import style from "./style.module.css";
 
 export function Card({ beer }) {
   return (
-
     <div className={style.beercard}>
-    <div className="card-group">
+      <div className="card-group">
+        <div className="card">
+          <img
+            src={`${beer.logo}`}
+            className="card-img-top"
+            style={{ height: "auto" }}
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{beer.name}</h5>
 
-      <div className="card">
-        <img
-          src={`${beer.logo}`}
-          className="card-img-top"
-          style={{ height: "400px" }}
-          alt="..."
-        />
-        <div className="card-body">
-          <h5 className="card-title">{beer.name}</h5>
+            <div className="card-text">
+              <div>IBU: {beer.ibu}</div>
+              <div>{beer.style}</div>
+            </div>
 
-          <div className="card-text">
-            <div>IBU: {beer.ibu}</div>
-            <div>{beer.style}</div>
-          </div>
-
-          <div className="card-text">
-            <small className="text-muted">
-              <span>{beer.country}</span>
-            </small>
+            <div className="card-text">
+              <small className="text-muted">
+                <span>{beer.country}</span>
+              </small>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/Components/SearchBar/index.js
+++ src/Components/SearchBar/index.js
@@ -1,12 +1,49 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Select from "react-select";
+import { useNavigate } from "react-router-dom";
+import style from "./style.module.css";
+
 export function SearchBar() {
+  const [beerList, setBeerList] = useState([]);
+  const navigate = useNavigate();
+
+  function handleSelect(e) {
+    navigate(`/beer-detail/${e.value}`);
+  }
+
+  function styleFunction(provided, state) {
+    return { ...provided, color: state.isFocused ? "blue" : "red" };
+  }
+
+  useEffect(() => {
+    async function fetchBeer() {
+      try {
+        const response = await axios.get(
+          `https://ironrest.herokuapp.com/mocked-beers`
+        );
+        setBeerList(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchBeer();
+  }, []);
+
+  const options = beerList.map((current) => {
+    return {
+      value: `${current._id}`,
+      label: current.name,
+    };
+  });
+
   return (
-    <div>
-      <label htmlFor="search" />
-      <input
-        name="search"
-        id="search-bar"
-        type="text"
-        placeholder="Procure aqui"
+    <div className={style.select}>
+      <Select
+        options={options}
+        onChange={handleSelect}
+        styles={styleFunction}
+        placeholder="Procurar cerveja perfeita..."
       />
     </div>
   );
